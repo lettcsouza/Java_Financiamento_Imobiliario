7dada8a1cc7669c12e722ae84f42110af0dd9d9e
pedro tavares <pedrotavaresbol1@gmail.com>
2024-06-20 16:21:20 -0300

    update arraylist

--- Main.java
+++ Main.java
@@ -1,18 +1,29 @@
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        InterfaceUsuario interfaceUsuario = new InterfaceUsuario();
-        
-        double valorImovel = interfaceUsuario.pedirValorImovel();
-        int prazoFinanciamento = interfaceUsuario.pedirPrazoFinanciamento();
-        double taxaJurosAnual = interfaceUsuario.pedirTaxaJuros();
+        ArrayList<Financiamento> financiamentos = new ArrayList<>();
 
-        Financiamento financiamento = new Financiamento(valorImovel, prazoFinanciamento, taxaJurosAnual);
+        financiamentos.add(new Financiamento(200000, 20, 5.0)); 
+        financiamentos.add(new Financiamento(300000, 25, 6.0)); 
+        financiamentos.add(new Financiamento(150000, 15, 4.5)); 
+        financiamentos.add(new Financiamento(250000, 30, 5.5));
 
-        double pagamentoMensal = financiamento.calcularParcelaMensal();
-        double totalPagamento = financiamento.calcularPagamentoTotal();
+        double totalValorImoveis = 0;
+        double totalValorFinanciamentos = 0;
 
-        System.out.printf("O valor do pagamento mensal é: R$ %.2f%n", pagamentoMensal);
-        System.out.printf("O valor do pagamento ao final do financiamento é: R$ %.2f%n", totalPagamento);
+        for (Financiamento financiamento : financiamentos) {
+            double valorImovel = financiamento.getValorImovel();
+            double valorFinanciamento = financiamento.calcularPagamentoTotal();
 
+            totalValorImoveis += valorImovel;
+            totalValorFinanciamentos += valorFinanciamento;
+
+            System.out.printf("Financiamento - valor do imóvel: R$ %.2f, valor do financiamento: R$ %.2f%n", valorImovel, valorFinanciamento);
+        }
+
+        System.out.printf("Total de todos os imóveis: R$ %.2f%n", totalValorImoveis);
+        System.out.printf("Total de todos os financiamentos: R$ %.2f%n", totalValorFinanciamentos);
     }
 }
